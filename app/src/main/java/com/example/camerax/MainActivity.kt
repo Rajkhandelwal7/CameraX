0b3a9337fd20a06534b223353a7f9617bf10abb4
Rajkhandelwal7 <62591280+Rajkhandelwal7@users.noreply.github.com>
2021-01-28 00:07:34 +0530

    cameraX

--- app/src/main/java/com/example/camerax/MainActivity.kt
+++ app/src/main/java/com/example/camerax/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.camerax
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraX
+import androidx.camera.core.impl.PreviewConfig
 import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED)
-        {
 
-        }else{
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),1234)
+        btn.setOnClickListener{
+            val intent=Intent(this,CameraActivity::class.java)
+            startActivity(intent)
+            finish()
         }
+
     }
 }
